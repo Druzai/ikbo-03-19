8fdaf5d9432ca613da809fc7e0ff3ffb1c33749b
Druzai <j7world@yandex.ru>
2020-09-08 20:01:07 +0300

    Updated practika3

--- practika3/MovableRectangle.java
+++ practika3/MovableRectangle.java
@@ -7,6 +7,10 @@ public class MovableRectangle extends MovablePoint {
         bottomRight = new MovablePoint(x2, y2, xSpeed, ySpeed);
     }
 
+    public boolean speedEqual(){
+        return topLeft.xSpeed == bottomRight.xSpeed && topLeft.ySpeed == bottomRight.ySpeed;
+    }
+
     @Override
     public String toString() {
         return "MovableRectangle{" +
